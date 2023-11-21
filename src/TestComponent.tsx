fa074f6fe2b5c58fcc00ab308baf85d950c90f16
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-21 12:04:45 +0100

    feat: first component

---
+++ src/TestComponent.tsx
@@ -0,0 +1,13 @@
+function TestComponent() {  
+    return (
+      <>
+        <div>
+          <p>
+            Ceci est un test test de création d'un nouveau composant
+          </p>
+        </div>
+      </>
+    )
+  }
+  
+  export default TestComponent
