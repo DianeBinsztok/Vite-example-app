e9d6c47e619c468deb1701328f4496d52e1d3cfb
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-22 13:00:37 +0100

    refactor: gather components in component folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TestComponent from './TestComponent'
+import TestComponent from './components/TestComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
