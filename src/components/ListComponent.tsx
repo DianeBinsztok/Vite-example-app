66746087ef39af52e996a46a4de1e9a3e5b7f277
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-22 14:51:04 +0100

    feat: ListComponent 4: conditionnal rendering

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,29 +1,35 @@
-const items = [
-    {
-        id:0,
-        name:"Pomme"
-    }, 
-    {
-        id:1,
-        name:"Poire"
-    }, 
-    {
-        id:2,
-        name:"Mangue"
-    }, 
-    {
-        id:3,
-        name:"Framboise"
-    }, 
-    {
-        id:4,
-        name:"Banane"
-    }, 
-    
-];
 
 
 function ListComponent(){
+    let items = [
+        {
+            id:0,
+            name:"Pomme"
+        }, 
+        {
+            id:1,
+            name:"Poire"
+        }, 
+        {
+            id:2,
+            name:"Mangue"
+        }, 
+        {
+            id:3,
+            name:"Framboise"
+        }, 
+        {
+            id:4,
+            name:"Banane"
+        }, 
+        
+    ];
+
+items = [];
+    
+    if(items.length === 0){
+        return(<p>La liste est vide pour le moment</p>);
+    }
     return(
         <ul>
             {items.map(item => <li key={item.id}>{item.name}</li>)}
