2e0de48891ad8e9a2e7e449dc27e2b0326e3cf59
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-22 13:30:44 +0100

    feat: ListComponent 3: give a unique key prop to each item

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,10 +1,32 @@
-const items = ["Pomme", "Poire", "Mangue", "Framboise", "Banane"];
+const items = [
+    {
+        id:0,
+        name:"Pomme"
+    }, 
+    {
+        id:1,
+        name:"Poire"
+    }, 
+    {
+        id:2,
+        name:"Mangue"
+    }, 
+    {
+        id:3,
+        name:"Framboise"
+    }, 
+    {
+        id:4,
+        name:"Banane"
+    }, 
+    
+];
 
 
 function ListComponent(){
     return(
         <ul>
-            {items.map(item => <li>{item}</li>)}
+            {items.map(item => <li key={item.id}>{item.name}</li>)}
         </ul>
 
     );
