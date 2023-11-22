97ec70c4b3fbc579afc1f023ed8bfbbe10737ddb
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-22 15:36:06 +0100

    feat: Handling events 3: with a function

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,5 +1,9 @@
 function ListComponent(){
     let items = ["Pomme","Poire","Mangue","Framboise","Banane"];
+
+    function handleClickEvent(){
+        console.log("pouet");
+    }
     
     if(items.length === 0){
         return(<p>La liste est vide pour le moment</p>);
@@ -8,9 +12,7 @@ function ListComponent(){
         <ul>
             {items.map((item, index) => 
             <li key={index}
-                onClick={(event)=>console.log(event)
-                /*renverra SyntheticBaseEvent => c'est une classe React qui encapsule l'objet event du navigateur. Elle permet d'assurer le même comportement de tous les navigateurs*/
-                }
+                onClick={()=>handleClickEvent()}
             >
                 {index} - {item}
             </li>)}
