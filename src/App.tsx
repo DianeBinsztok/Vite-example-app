17edb1135025024b364c5f046ec632d725bffc3f
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-23 13:50:02 +0100

    Declare an array of objects in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import './App.css'
 import ApplicationsList from './components/ApplicationsList'
 
+let myItems = [
+  {
+      title:"Testeur de matelas",
+      company:"LaBoite",
+      city:"Okinawa",
+      contract:"CDI",
+      status:"À relancer"
+  },
+  {
+      title:"Goûteur de desserts",
+      company:"TheCompany",
+      city:"Bordeaux",
+      contract:"CDD",
+      status:"En cours"
+  },
+  {
+      title:"Câlineur de chiots",
+      company:"Trucmuche",
+      city:"Valence",
+      contract:"CDI",
+      status:"En cours"
+  }
+];
+
 function App() {
 
   return (
     <>
     <h1>Mon appli en React:</h1>
-    <ApplicationsList/>
+    <ApplicationsList 
+    heading="Liste des candidatures en cours:"
+    items = {myItems}/>
     </>
   )
 }
--- src/components/ApplicationsList.tsx
+++ src/components/ApplicationsList.tsx
@@ -1,44 +1,23 @@
 import Application from "./Application";
-function ApplicationsList(){
-    let applications = [
-        {
-            title:"Testeur de matelas",
-            company:"LaBoite",
-            city:"Okinawa",
-            contract:"CDI",
-            status:"À relancer"
-        },
-        {
-            title:"Goûteur de desserts",
-            company:"TheCompany",
-            city:"Bordeaux",
-            contract:"CDD",
-            status:"En cours"
-        },
-        {
-            title:"Câlineur de chiots",
-            company:"Trucmuche",
-            city:"Valence",
-            contract:"CDI",
-            status:"En cours"
-        }
-    ];
-    
-    if(applications.length === 0){
+
+/*Pour renseigner un type 'tableau d'objets' en typescript (ici: items), il faut le déstructurer dans les paramètres et typer les propriété de l'objet. Se contenter de la mention 'object[]' ne suffira pas.*/ 
+function ApplicationsList({heading, items}:{heading:string, items:{title:string,company:string,city:string,contract:string,status:string,}[]}){
+
+    if(items.length === 0){
         return(<p>La liste est vide pour le moment</p>);
     }
     return(
         <>
-            <h2>Liste des candidatures en cours:</h2>
+            <h2>{heading}</h2>
             <ul>
-                {applications.map((application, index) => 
+                {items.map((item, index) => 
                 <Application 
                 key={index} 
-                title={application.title}
-                company = {application.company}
-                city = {application.city}
-                contract = {application.contract}
-                status = {application.status}/>
+                title = {item.title}
+                company = {item.company}
+                city = {item.city}
+                contract = {item.contract}
+                status = {item.status}/>
                 )}
             </ul>
         </>
