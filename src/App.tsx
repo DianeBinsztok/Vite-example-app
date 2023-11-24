2d1cfc1ab218d4291f7956a6c8d8b99603edaa72
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-11-24 13:30:20 +0100

    Optional props 1: just a button and some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css'
-import Message from './components/Message';
+import Button from './components/Button';
 
 function App() {
 
-  /*J'utilise Message comme une double balise avec du contenu enfant*/
   return (
-    <Message>
-        <h1>Ceci est un test</h1>
-        <p>L'enfant du composant Message est à présent composé d'éléments HTML</p> 
-        <p>Son type n'est donc plus <strong>'string'</strong> mais <strong>un noeud du DOM</strong></p>
-    </Message>
+    <Button/>
   )
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+function Button(){
+    
+    return(
+        <button className="btn">
+            Ceci est un bouton!
+        </button>
+
+    );
+}
+export default Button;
